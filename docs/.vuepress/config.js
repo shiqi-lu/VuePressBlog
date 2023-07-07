c5e52d9263a15e23d021585c90d699ef2a2d910a
Traum Lou <traumlou@gmail.com>
2023-07-07 18:41:50 +0800

    调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,13 +4,13 @@ import mathjax3 from 'markdown-it-mathjax3'
 export default defineUserConfig({
     lang: 'zh-CN',
     title: '代码天涯路',
-    description: '这是我的第一个 VuePress 站点',
+    description: '该博客用于记录我的成长记录，内容包括CS、AI、数学、自我提升',
     extendsMarkdown: md => {
         md.use(mathjax3)
     },
     theme: defaultTheme({
         logo: 'https://img.shiqi-lu.tech/47logo1.jpg',
-        // 默认主题配置
+        contributors: false,
         navbar: [
             {
                 text: '首页',
@@ -20,16 +20,12 @@ export default defineUserConfig({
         sidebar: [
             {
                 text: '编程语言',
-                link: '/编程语言/',
+                link: '/language/',
                 children: [
-                    // SidebarItem
                     {
                         text: '第一篇',
-                        link: '/编程语言/first.md',
-                        //   children: [],
+                        link: '/language/first.md',
                     },
-                    // 字符串 - 页面文件路径
-                    // '/foo/bar.md',
                 ],
             },
         ],
--- docs/编程语言/first.md
+++
@@ -1,8 +0,0 @@
-# first page
-hahaha
-aaaaaaaaaaaaaaaa
-
-
-## 不错
-
-$$ m^ $$
